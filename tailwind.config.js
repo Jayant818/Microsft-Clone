0599cf7a01bad08667ad3413d3df461e3e3c0989
Jayant <jayant@errsole.com>
2022-09-24 14:42:56 +0530

    Add Tailwind Starter

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+// If you want some changes in the default config, you can copy from full config file & after changes add that in the Extend Section.
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
